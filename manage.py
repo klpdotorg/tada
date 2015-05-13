244a4183104306b7c437360ee0636a49842c18b9
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-05-13 23:27:57 +0530

    Adds the apps folder to sys path

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tada.settings")
+    sys.path.append(os.path.join(os.getcwd(), 'apps'))
 
     from django.core.management import execute_from_command_line
 
