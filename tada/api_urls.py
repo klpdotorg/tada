83b8db8c6bd9b4842c3858925b3c99d607c4bc3e
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-05-21 12:58:36 +0530

    Adds institution endpoint

--- tada/api_urls.py
+++ tada/api_urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, url
 
+from schools.api_views import (
+    InstitutionDetailView,
+)
 
 urlpatterns = patterns(
     '',
+    url('^institution/(?P<pk>[0-9]+)$', InstitutionDetailView.as_view(), name='api_institution_detail_view'),
 )
