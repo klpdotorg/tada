b3640086a9d0f9265630b1b2cf9a73241a17f10f
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-05-21 09:55:00 +0530

    Includes api_urls in urls

--- tada/urls.py
+++ tada/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tada.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/v1/', include('tada.api_urls')),
 )
