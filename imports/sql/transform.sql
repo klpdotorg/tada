e922285cdb82674bdc182c28dbf3fdcae60a6367
Vinayak Hegde <vinayakh@gmail.com>
2015-05-23 02:39:46 +0530

    Added transformation for schools_academic_year

---
+++ imports/sql/transform.sql
@@ -0,0 +1,4 @@
+-- schools_academic_year
+ALTER TABLE ems_schools_academic_year TO schools_academic_year;
+ALTER TABLE schools_academic_year ADD start_year smallint; 
+ALTER TABLE schools_academic_year ADD end_year smallint; 
