bbc6e8c5e609b832681be6efcab0f8f35c059a44
Vinayak Hegde <vinayakh@gmail.com>
2015-05-23 15:41:55 +0530

    Added start and end year to academic year

--- imports/sql/transform.sql
+++ imports/sql/transform.sql
@@ -1,4 +1,6 @@
 -- schools_academic_year
 ALTER TABLE ems_schools_academic_year TO schools_academic_year;
 ALTER TABLE schools_academic_year ADD start_year smallint; 
-ALTER TABLE schools_academic_year ADD end_year smallint; 
+ALTER TABLE schools_academic_year ADD end_year smallint;
+update schools_academic_year set start_year = (LEFT(name,4))::int2
+update schools_academic_year set end_year = (RIGHT(name,4))::int2;` 
