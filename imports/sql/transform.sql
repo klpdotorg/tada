de0b197085c11c55bcff656172df9d10ed3c7140
Vinayak Hegde <vinayakh@gmail.com>
2015-05-28 01:34:22 +0530

    Merge institution and institution address tables

--- imports/sql/transform.sql
+++ imports/sql/transform.sql
@@ -77,7 +77,7 @@ ON a.assessment_id = b.id WHERE b.typ = 3;
 -- Step 3: Merge child and student tables See issue#19
 -- schools_relations
 -- replace child_id with student_id 
-SELECT a.id, a.relation_type, a.first_name, a.middle_name, a.last_name, b.id
+INSERT INTO schools_relations SELECT a.id, a.relation_type, a.first_name, a.middle_name, a.last_name, b.id
 FROM ems_schools_relations a 
 INNER JOIN ems_schools_student b 
 ON a.child_id = b.child_id;
@@ -93,7 +93,15 @@ on a.child_id = b.id
 
 -- Step 4: Merge Institution and Institution address tables  
 -- schools_institution
+-- merge the institution address with this table
+
+INSERT INTO schools_institution SELECT a.id, a.dise_code, a.name, a.institution_gender, a.active, a.boundary_id, a.cat_id, a.mgmt_id, b.address, b.area, b.instidentification, b.instidentification2, b.landmark, b.pincode, b.route_information 
+FROM ems_schools_institution a 
+INNER JOIN ems_schools_institution_address b 
+ON a.inst_address_id = b.id;
+ 
 -- schools_institution_address
+-- drop this table later
 
 -- Step 5: Import and Update Staff tables 
 -- schools_staff
