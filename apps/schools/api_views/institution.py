a1d3d2e9c9098c2ce454a612c1545728241fbf8a
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-08-03 12:56:14 +0530

    Implements the InstitutionViewSet

--- apps/schools/api_views/institution.py
+++ apps/schools/api_views/institution.py
@@ -1,18 +1,14 @@
-from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics, viewsets
 
 from schools.serializers import (
     InstitutionSerializer,
 )
-
 from schools.models import (
     Institution,
 )
 
-
-class InstitutionDetailView(generics.RetrieveUpdateDestroyAPIView):
+class InstitutionViewSet(viewsets.ModelViewSet):
+    queryset = Institution.objects.all()
     serializer_class = InstitutionSerializer
-
-    def get_object(self):
-        return Institution.objects.get(id=self.kwargs['pk'])
