58c103e447f492dfdd4cdf40f36930f4a34187c7
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-08-03 13:01:46 +0530

    Minor formatting on serializers

--- apps/schools/serializers.py
+++ apps/schools/serializers.py
@@ -18,11 +18,17 @@ class RelationsSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Relations
-        fields = ('relation_type', 'first_name' ,'middle_name', 'last_name')
+        fields = (
+            'relation_type', 'first_name' ,'middle_name', 'last_name'
+        )
+
 
 class StudentSerializer(serializers.ModelSerializer):
     
     relations = RelationsSerializer(many='True', source='get_relations')
     class Meta:
         model = Student
-        fields = ('first_name' ,'middle_name', 'last_name' , 'uid', 'dob', 'gender' , 'mt', 'active', 'relations')
+        fields = (
+            'first_name', 'middle_name', 'last_name', 'uid', 'dob', 'gender',
+            'mt', 'active', 'relations'
+        )
