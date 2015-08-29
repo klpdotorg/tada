51c0e1331c8f417223b8fb398c6826f0be1d9824
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-08-29 20:55:45 +0530

    Fixes list -> last

--- apps/schools/filters.py
+++ apps/schools/filters.py
@@ -41,4 +41,4 @@ class StudentFilter(django_filters.FilterSet):
 
     class Meta:
         model = Student
-        fields = ['first_name', 'middle_name', 'list_name']
+        fields = ['first_name', 'middle_name', 'last_name']
