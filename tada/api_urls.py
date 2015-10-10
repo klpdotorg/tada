99e2e63bd624be1b7511c7aea87a5c728d949692
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-10-10 12:52:03 +0530

    Does minor code formatting and lists the endpoints in alphabetical order

--- tada/api_urls.py
+++ tada/api_urls.py
@@ -3,20 +3,22 @@ from rest_framework_bulk.routes import BulkRouter
 
 from django.conf.urls import patterns, url
 
-from schools.api_views import (StaffViewSet,InstitutionViewSet, StudentViewSet,
-    AssessmentViewSet, ProgrammeViewSet, BoundaryViewSet, BoundaryTypeViewSet, BoundaryCategoryViewSet
+from schools.api_views import (
+    StaffViewSet,InstitutionViewSet, StudentViewSet,
+    AssessmentViewSet, ProgrammeViewSet, BoundaryViewSet,
+    BoundaryTypeViewSet, BoundaryCategoryViewSet
 )
 
 router = routers.SimpleRouter()
 bulkrouter = BulkRouter()
 bulkrouter.register(r'students', StudentViewSet, base_name='students')
 
-router.register(r'institutions', InstitutionViewSet, base_name='institution')
 router.register(r'assessments', AssessmentViewSet, base_name='assessment')
-router.register(r'programmes', ProgrammeViewSet, base_name='programme')
 router.register(r'boundaries', BoundaryViewSet, base_name='boundary')
-router.register(r'boundarytype', BoundaryTypeViewSet, base_name='boundarytype')
-router.register(r'boundarycategory', BoundaryCategoryViewSet, base_name='boundarycategory')
-router.register(r'staff',StaffViewSet, base_name='staff')
+router.register(r'boundarytypes', BoundaryTypeViewSet, base_name='boundarytype')
+router.register(r'boundarycategories', BoundaryCategoryViewSet, base_name='boundarycategory')
+router.register(r'institutions', InstitutionViewSet, base_name='institution')
+router.register(r'programmes', ProgrammeViewSet, base_name='programme')
+router.register(r'staff', StaffViewSet, base_name='staff')
 #router.register(r'students', StudentViewSet, base_name='students')
 urlpatterns = router.urls + bulkrouter.urls
