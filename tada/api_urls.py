9b098cde7c7d440eeeb9460b6595fd15f5bc48dc
Haris Ibrahim K. V <blucalvin@gmail.com>
2015-10-12 16:40:41 +0530

    Does minor code formatting

--- tada/api_urls.py
+++ tada/api_urls.py
@@ -24,16 +24,41 @@ router.register(
     InstitutionAssessmentViewSet,
     base_name='institution-assessment'
 )
-router.register(r'boundaries', BoundaryViewSet, base_name='boundary')
-router.register(r'boundarytypes', BoundaryTypeViewSet, base_name='boundarytype')
-router.register(r'boundarycategories', BoundaryCategoryViewSet, base_name='boundarycategory')
-router.register(r'institutions', InstitutionViewSet, base_name='institution')
-router.register(r'programmes', ProgrammeViewSet, base_name='programme')
-router.register(r'staff', StaffViewSet, base_name='staff')
+router.register(
+    r'boundaries',
+    BoundaryViewSet,
+    base_name='boundary'
+)
+router.register(
+    r'boundarytypes',
+    BoundaryTypeViewSet,
+    base_name='boundarytype'
+)
+router.register(
+    r'boundarycategories',
+    BoundaryCategoryViewSet,
+    base_name='boundarycategory'
+)
+router.register(
+    r'institutions',
+    InstitutionViewSet,
+    base_name='institution'
+)
+router.register(
+    r'programmes',
+    ProgrammeViewSet,
+    base_name='programme'
+)
+router.register(
+    r'staff',
+    StaffViewSet,
+    base_name='staff'
+)
 router.register(
     r'student-assessments',
     StudentAssessmentViewSet,
     base_name='student-assessment'
 )
+
 #router.register(r'students', StudentViewSet, base_name='students')
 urlpatterns = router.urls + bulkrouter.urls
