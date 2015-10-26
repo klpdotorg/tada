87a0ad9b6064a9095efab5aee8261585243c3085
Vinayak Hegde <vinayakh@gmail.com>
2015-10-26 11:19:44 +0530

    Added migration for temp answers and compensation log

---
+++ apps/schools/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('schools', '0010_auto_20150903_0947'),
+        ('schools', '0007_compensationauditlog'),
+    ]
+
+    operations = [
+    ]
