c2c87a75dd664b44bd9c1809d20994d57c6be29a
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-01-18 14:14:50 +0530

    Adds the StudentGroupViewSet

--- apps/schools/api_views/students.py
+++ apps/schools/api_views/students.py
@@ -1,22 +1,30 @@
-from rest_framework import generics,viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework_bulk import (BulkCreateModelMixin, ListBulkCreateUpdateDestroyAPIView,)
+from rest_framework import generics,viewsets
+from rest_framework_extensions.mixins import NestedViewSetMixin
+from rest_framework_bulk import BulkCreateModelMixin, ListBulkCreateUpdateDestroyAPIView
 
 from schools.filters import (
-    StudentFilter
+    StudentFilter,
+    StudentGroupFilter
 )
 from schools.serializers import (
     StudentSerializer,
+    StudentGroupSerializer
 )
 from schools.models import (
-    Student, 
+    Student,
+    StudentGroup
 )
 
-''' Returns the list of students. Paginates by 10. Set this globally in settings?'''
+
 class StudentViewSet(BulkCreateModelMixin, viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     filter_class = StudentFilter
 
 
+class StudentGroupViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
+    queryset = StudentGroup.objects.all()
+    serializer_class = StudentGroupSerializer
+    filter_class = StudentGroupFilter
