811a74dd330f1ca06e75b0fa87d3c1651722f229
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-02-20 22:15:07 +0530

    Implements the accounts.serializers file. Overrides the UserSerializer and addes the 'groups' field to it

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from django.contrib.auth.models import Group
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class GroupSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model = Group
+        fields = ('name')
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    groups = GroupSerializer(many=True)
+
+    class Meta:
+        model = User
+        fields = tuple(User.REQUIRED_FIELDS) + (
+            User._meta.pk.name,
+            User.USERNAME_FIELD,
+            'groups',
+        )
+        read_only_fields = (
+            User.USERNAME_FIELD,
+        )
