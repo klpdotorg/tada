535ab63fb74319f3dfed126bb3c58aa682ad117a
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-02-20 22:11:16 +0530

    Points auth endpoint to accounts.urls

--- tada/urls.py
+++ tada/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns(
     '',
-    url(r'^auth/', include('djoser.urls.authtoken')),
+    url(r'^auth/', include('accounts.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/v1/', include('tada.api_urls')),
     url(r'^api/docs/', include('rest_framework_swagger.urls')),
