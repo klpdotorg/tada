6df2b33c32ee9e26cb875007d7fdf1a00e6b6ee8
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-02-20 22:13:21 +0530

    Overrides the djoser UserView

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+
+from accounts.serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserView(generics.RetrieveUpdateAPIView):
+    """
+    Use this endpoint to retrieve/update user.
+    """
+    model = User
+    serializer_class = UserSerializer
+
+    def get_object(self, *args, **kwargs):
+        return self.request.user
