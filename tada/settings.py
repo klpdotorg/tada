c30b12971bdc3c3c607b620fb94a9e20ea8ec034
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-03-05 13:36:07 +0530

    Adds django-guardian to settings

--- tada/settings.py
+++ tada/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    # In-built apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -37,13 +38,16 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts',
-    'common',
+    # Third party apps
     'django_extensions',
     'djoser',
     'rest_framework',
-    'rest_framework.authtoken',
     'rest_framework_swagger',
+    'rest_framework.authtoken',
+
+    # Local apps
+    'accounts',
+    'common',
     'schools',
     'tests',
 )
@@ -115,7 +119,8 @@ REST_FRAMEWORK = {
 
 AUTHENTICATION_BACKENDS = (
     'accounts.backends.EmailOrUsernameModelBackend',
-    'django.contrib.auth.backends.ModelBackend'
+    'django.contrib.auth.backends.ModelBackend',
+    'guardian.backends.ObjectPermissionBackend',
 )
 
 CORS_ORIGIN_ALLOW_ALL = True
