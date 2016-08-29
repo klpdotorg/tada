9296fd9dca095dee5c19f945be6b62af81b44f21
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-08-29 11:43:21 +0530

    Drops the comments
    Signed-off-by: Haris Ibrahim K. V <blucalvin@gmail.com>

--- apps/accounts/migrations/0001_initial.py
+++ apps/accounts/migrations/0001_initial.py
@@ -9,32 +9,16 @@ from django.contrib.auth.models import Group , Permission
 
 def forwards_func(apps, schema_editor):
     group = Group.objects.create(name="tada_admin")
-    permissions = Permission.objects.all()
-    for permission in permissions:
+    for permission in Permission.objects.all():
         group.permissions.add(permission)
+
     group = Group.objects.create(name="tada_deo")
     group = Group.objects.create(name="tada_dee")
-        
-    # We get the model from the versioned app registry;
-    # if we directly import it, it'll be the wrong version
-    # Country = apps.get_model("myapp", "Country")
-    # db_alias = schema_editor.connection.alias
-    # Country.objects.using(db_alias).bulk_create([
-    #     Country(name="USA", code="us"),
-    #     Country(name="France", code="fr"),
-    # ])
 
 def reverse_func(apps, schema_editor):
     Group.objects.get(name="tada_admin").delete()
     Group.objects.get(name="tada_deo").delete()
     Group.objects.get(name="tada_dee").delete()
-    # forwards_func() creates two Country instances,
-    # so reverse_func() should delete them.
-    # Country = apps.get_model("myapp", "Country")
-    # db_alias = schema_editor.connection.alias
-    # Country.objects.using(db_alias).filter(name="USA", code="us").delete()
-    # Country.objects.using(db_alias).filter(name="France", code="fr").delete()    
-
 
 class Migration(migrations.Migration):
 
