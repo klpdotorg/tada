5d24ac9b12573fc1d6dc0c314217d5a64c2a951d
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-09-19 12:10:52 +0530

    Implements both 'add_to_groups' and 'remove_from_groups'  data parameters

--- apps/accounts/api_views.py
+++ apps/accounts/api_views.py
@@ -17,21 +17,36 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
     def update(self, request, *args, **kwargs):
-        group_ids = request.data.pop('groups', None)
+        add_to_group_ids = request.data.pop('add_to_groups', [])
+        remove_from_group_ids = request.data.pop('remove_from_groups', [])
         user = self.get_object()
-        if group_ids:
-            try:
-                groups = [Group.objects.get(id=group_id) for group_id in group_ids]
-            except:
-                raise APIException('One or more of the given group IDs are invalid')
-            for group in groups:
-                user.groups.add(group)
-
+        self.update_groups(
+            user,
+            add_to_group_ids,
+            remove_from_group_ids
+        )
         partial = kwargs.pop('partial', False)
         serializer = self.get_serializer(user, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return Response(serializer.data)
+
+    def update_groups(self, user, add_to_group_ids, remove_from_group_ids):
+        try:
+            groups = [
+                Group.objects.get(id=group_id)
+                for group_id in add_to_group_ids + remove_from_group_ids
+            ]
+        except:
+            raise APIException('One or more of the given group IDs are invalid')
+
+        if add_to_group_ids:
+            for group in add_to_group_ids:
+                user.groups.add(group)
+
+        if remove_from_group_ids:
+            for group in remove_from_group_ids:
+                user.groups.remove(group)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
