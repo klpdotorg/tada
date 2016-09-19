bed8685a1a875e5c199e1c69dc3193ce00625ff7
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-09-19 10:49:58 +0530

    Imports Response

--- apps/accounts/api_views.py
+++ apps/accounts/api_views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.exceptions import APIException
 
 from django.contrib.auth import get_user_model
