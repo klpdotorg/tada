eb0fe64c421c87eca98ed3f70acad233ff57530d
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-12-01 01:03:52 +0530

    Implements the update() method on UserViewSet and allows updation of password

--- apps/accounts/api_views.py
+++ apps/accounts/api_views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from django.contrib.auth import get_user_model
 
@@ -10,3 +11,17 @@ User = get_user_model()
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        password = request.data.get('password', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        if password:
+            instance.set_password(password)
+            instance.save()
+
+        return Response(serializer.data)
+
