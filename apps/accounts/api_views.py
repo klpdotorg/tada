70d05954c08f8f67658a0d18b4c37578e069784f
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-11-30 20:55:23 +0530

    Implements UserViewSet and puts it in api_views.py

---
+++ apps/accounts/api_views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+
+from django.contrib.auth import get_user_model
+
+from accounts.serializers import UserSerializer
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
