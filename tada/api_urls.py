846663146512f932bf385bcfbab9252830825c42
Haris Ibrahim K. V <blucalvin@gmail.com>
2016-12-29 13:20:57 +0530

    Implements the /enrollment endpoint for StudentGroup to Student and viceversa mapping

--- tada/api_urls.py
+++ tada/api_urls.py
@@ -16,75 +16,91 @@ from schools.api_views import (
     StaffViewSet,
     StudentViewSet,
     StudentGroupViewSet,
+    StudentStudentGroupViewSet,
 )
 
-router = routers.SimpleRouter()
 bulkrouter = BulkRouter()
+router = routers.SimpleRouter()
 nested_router = ExtendedSimpleRouter()
 
-bulkrouter.register(r'students', StudentViewSet, base_name='students')
+router.register(r'boundaries', BoundaryViewSet, base_name='boundary')
+router.register(r'boundarycategories', BoundaryCategoryViewSet, base_name='boundarycategory')
+router.register(r'boundarytypes', BoundaryTypeViewSet, base_name='boundarytype')
 
-router.register(
-    r'boundaries',
-    BoundaryViewSet,
-    base_name='boundary'
-)
-router.register(
-    r'boundarycategories',
-    BoundaryCategoryViewSet,
-    base_name='boundarycategory'
-)
-router.register(
-    r'boundarytypes',
-    BoundaryTypeViewSet,
-    base_name='boundarytype'
-)
-
-# Nested Routers
-
+## Institution -> StudentGroup -> Students
 nested_router.register(
     r'institutions',
     InstitutionViewSet,
     base_name='institution'
-).register(
-    r'studentgroups',
-    StudentGroupViewSet,
-    base_name='studentgroup',
-    parents_query_lookups=['institution']
-).register(
-    r'students',
-    StudentViewSet,
-    base_name='nested_students',
-    parents_query_lookups=['studentgroups__institution', 'studentgroups']
-)
+    ).register(
+        r'studentgroups',
+        StudentGroupViewSet,
+        base_name='studentgroup',
+        parents_query_lookups=['institution']
+        ).register(
+            r'students',
+            StudentViewSet,
+            base_name='nested_students',
+            parents_query_lookups=['studentgroups__institution', 'studentgroups']
+        )
 
+router.register(r'languages', LanguageViewSet, base_name='language')
+
+## Programmes -> Assessments -> Questions
 nested_router.register(
     r'programmes',
     ProgrammeViewSet,
     base_name='programme'
-).register(
-    r'assessments',
-    AssessmentViewSet,
-    base_name='assessment',
-    parents_query_lookups=['programme']
-).register(
-    r'questions',
-    QuestionViewSet,
-    base_name='question',
-    parents_query_lookups=['assessment__programme', 'assessment']
-)
+    ).register(
+        r'assessments',
+        AssessmentViewSet,
+        base_name='assessment',
+        parents_query_lookups=['programme']
+        ).register(
+            r'questions',
+            QuestionViewSet,
+            base_name='question',
+            parents_query_lookups=['assessment__programme', 'assessment']
+        )
 
-router.register(
-    r'staff',
-    StaffViewSet,
-    base_name='staff'
-)
+router.register(r'staff', StaffViewSet, base_name='staff')
 
-router.register(
-    r'languages',
-    LanguageViewSet,
-    base_name='language'
-)
+## StudentGroups -> Students -> StudentStudentGroupRelation
+nested_router.register(
+    r'studentgroups',
+    StudentGroupViewSet,
+    base_name='studentgroup',
+    ).register(
+        r'students',
+        StudentViewSet,
+        base_name='nested_students',
+        parents_query_lookups=['studentgroups']
+        ).register(
+            r'enrollment',
+            StudentStudentGroupViewSet,
+            base_name='studentstudentgrouprelation',
+            parents_query_lookups=['student__studentgroups', 'student']
+        )
 
-#router.register(r'students', StudentViewSet, base_name='students')
+##  Students -> StudentGroups -> StudentStudentGroupRelation
+nested_router.register(
+    r'students',
+    StudentViewSet,
+    base_name='students',
+    ).register(
+        r'studentgroups',
+        StudentGroupViewSet,
+        base_name='nested_students',
+        parents_query_lookups=['students']
+        ).register(
+            r'enrollment',
+            StudentStudentGroupViewSet,
+            base_name='studentstudentgrouprelation',
+            parents_query_lookups=['student_id', 'student_group']
+        )
+
+#http://localhost:8000/api/v1/studentgroups/3525154/students/2294002/enrollment
+
+bulkrouter.register(r'students', StudentViewSet, base_name='students')
+
 urlpatterns = router.urls + bulkrouter.urls + nested_router.urls
