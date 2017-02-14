074221af85f1f25b44631ff4618707e2c5af835d
Haris Ibrahim K. V <blucalvin@gmail.com>
2017-02-14 11:03:16 +0530

    Implements patching a User to add them to a group

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
@@ -14,6 +15,7 @@ class GroupSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
 
+    group = serializers.CharField(write_only=True, allow_blank=True)
     groups = GroupSerializer(many=True, required=False)
 
     class Meta:
@@ -21,10 +23,28 @@ class UserSerializer(serializers.ModelSerializer):
         fields = tuple(User.REQUIRED_FIELDS) + (
             User._meta.pk.name,
             User.USERNAME_FIELD,
+            'group',
             'groups',
             'first_name',
             'last_name',
         )
         read_only_fields = (
             User.USERNAME_FIELD,
+            'groups'
         )
+
+    def update(self, instance, validated_data):
+        group_name = validated_data.pop('group', None)
+
+        if group_name:
+            try:
+                group = Group.objects.get(name=group_name)
+                group.user_set.add(instance)
+            except:
+                raise ValidationError(group_name + " not found.")
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
