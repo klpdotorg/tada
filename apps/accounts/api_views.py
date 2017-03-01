df3783756048409ba675d1ebaff82b904961d521
Haris Ibrahim K. V <blucalvin@gmail.com>
2017-03-01 22:12:29 +0530

    Implements error handling for permissions

--- apps/accounts/api_views.py
+++ apps/accounts/api_views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import APIException
+
 from guardian.shortcuts import assign_perm
 
 from django.contrib.auth.models import Group
@@ -9,6 +11,7 @@ from django.contrib.auth import get_user_model
 
 from .filters import UserFilter
 from .serializers import GroupSerializer, UserSerializer
+from .permissions import HasAssignPermPermission
 
 from schools.models import (
     Assessment,
@@ -45,22 +48,38 @@ class GroupViewSet(viewsets.ModelViewSet):
 
 
 class AssignPermissionView(APIView):
-    permission_classes = (permissions.HasAssignPermPermission,)
+    permission_classes = (HasAssignPermPermission,)
     
     def post(self, request):
         user_id = self.request.data.get('user_id', None)
-        # school_id = self.request.data.get('school_id', None)
+        institution_id = self.request.data.get('institution_id', None)
         boundary_id = self.request.data.get('boundary_id', None)
         assessment_id = self.request.data.get('assesment_id', None)
 
-        user_to_be_permitted = User.objects.get(id=user_id)
+        try:
+            user_to_be_permitted = User.objects.get(id=user_id)
+        except Exception as ex:
+            raise APIException(ex)
+
+        if institution_id:
+            try:
+                institution = Institution.objects.get(id=institution_id)
+            except Exception as ex:
+                raise APIException(ex)
+            assign_perm('change_institution', user_to_be_permitted, institution)
 
         if assessment_id:
-            assessment = Assessment.objects.get(id=assessment_id)
+            try:
+                assessment = Assessment.objects.get(id=assessment_id)
+            except Exception as ex:
+                raise APIException(ex)
             assign_perm('change_assessment', user_to_be_permitted, assessment)
 
         if boundary_id:
-            boundary = Boundary.objects.get(id=boundary_id)
+            try:
+                boundary = Boundary.objects.get(id=boundary_id)
+            except Exception as ex:
+                raise APIException(ex)
             assign_perm('change_boundary', user_to_be_permitted, boundary)
 
             institutions_under_boundary = boundary.institutions()
