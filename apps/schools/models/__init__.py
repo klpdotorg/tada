21fb9c1eb8046e3792e3c327f9a64fc7afc4f191
Haris Ibrahim K. V <blucalvin@gmail.com>
2017-04-21 09:55:52 +0530

    Adds AnswerStudentGroup to __init__

--- apps/schools/models/__init__.py
+++ apps/schools/models/__init__.py
@@ -1,11 +1,30 @@
-from .boundary import (BoundaryCategory, BoundaryType, Boundary)
-from .institution import (InstitutionCategory, MoiType, InstitutionManagement,
-    StaffType, QualificationList,
-    Institution, AcademicYear, Staff, current_academic, default_end_date)
-from .assessments import (Assessment,
-    AssessmentStudentGroupAssociation, AssessmentInstitutionAssociation, Question,
-    AnswerStudent, AnswerInstitution)
-from .students import (Relations, StudentGroup, Student, 
-    StudentStudentGroupRelation)
-from .programs import (Programme)
-from .audit import (CompensationAuditLog)
+from .boundary import BoundaryCategory, BoundaryType, Boundary
+from .institution import (
+    InstitutionCategory,
+    MoiType,
+    InstitutionManagement,
+    StaffType,
+    QualificationList,
+    Institution,
+    AcademicYear,
+    Staff,
+    current_academic,
+    default_end_date
+)
+from .assessments import (
+    Assessment,
+    AssessmentStudentGroupAssociation,
+    AssessmentInstitutionAssociation,
+    Question,
+    AnswerStudent,
+    AnswerInstitution,
+    AnswerStudentGroup
+)
+from .students import (
+    Relations,
+    StudentGroup,
+    Student, 
+    StudentStudentGroupRelation
+)
+from .programs import Programme
+from .audit import CompensationAuditLog
