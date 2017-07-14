aa285a03ec5ee61f43e7fdb839e8b53c6a150916
Ansal S <ansal@bssatech.com>
2017-07-14 11:07:59 +0530

    teacher filter only active=2

--- apps/schools/api_views/teachers.py
+++ apps/schools/api_views/teachers.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 
 from accounts.permissions import WorkUnderInstitutionPermission
@@ -12,3 +13,11 @@ class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
     filter_class = TeacherFilter
+
+    def list(self, request):
+        queryset = Teacher.objects.filter(
+            active=2,
+            institution__pk=request.GET.get('institution', 0)
+        )
+        serializer = TeacherSerializer(queryset, many=True)
+        return Response(serializer.data)
