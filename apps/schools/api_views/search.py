176d3643d93bcb2c387f203fd268c3049d03b755
Ansal S <ansal@bssatech.com>
2017-07-17 11:41:35 +0530

    include boundary details in student search

--- apps/schools/api_views/search.py
+++ apps/schools/api_views/search.py
@@ -16,8 +16,24 @@ class SearchKLPViewSet(viewsets.ViewSet):
             id__contains=klp_id, active=2)[:3]
         students = Student.objects.filter(
             id__contains=klp_id, active=2)[:3]
+
+        students_json = []
+        for s in students:
+            data = StudentSerializer(s).data
+            if s.studentgroups.count():
+                sg = s.studentgroups.all()[0]
+                inst = sg.institution
+                data['boundary_details'] = {
+                    'student_group': sg.id,
+                    'institution': inst.id,
+                    'cluster': inst.boundary.id,
+                    'block': inst.boundary.parent.id,
+                    'district': inst.boundary.parent.parent.id
+                }
+            students_json.append(data)
+
         return Response({
             'institutions': InstitutionSerializer(
                 institutions, many=True).data,
-            'students': StudentSerializer(students, many=True).data
+            'students': students_json
         })
